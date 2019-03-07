2f5313af15778ec20913f6b355833a6f87e54a99
Maria <mariaprotas@yandex.ru>
2019-03-07 11:47:17 +0300

    read data from file

--- filereader.py
+++ filereader.py
@@ -1,5 +1,17 @@
 import numpy as np
+import re
 
-x = np.fromfile('data.dat')
-print(['{0:.20f}'.format(i) for i in x])
-y = np.loadtxt('data.dat')
+def read_data(filename):
+    with open(filename, 'r') as myfile:
+        data = myfile.read()
+    text_info = re.findall('[a-zA-Z.]+', data)
+    data = re.sub(',', '.', data)
+    text_info = list(text_info)
+    columns = text_info[:2]
+    units = text_info[2:4]
+    data = re.split('[\s]', data)[4:]
+    data_x = data[::2]
+    data_y = data[1::2]
+    return [data_x, data_y, columns, units]
+
+print(read_data('data.dat'))
